b7b7230cb31cb19346d89ae3341362e5da50edc5
HAN <88082769+Hahannn@users.noreply.github.com>
2023-05-23 22:04:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var flag = 1;
+
+function f1() {
+    alert("AWWWW");
+}
+
+function f() {
+    var Bn = document.getElementById("Bn"); // Replace "yourElementId" with the actual ID of your element
+
+    if (flag == 1) {
+        Bn.style.top = calculateResponsiveValue(100, 'top');
+        Bn.style.left = calculateResponsiveValue(1030, 'left');
+        flag = 2;
+    } else if (flag == 2) {
+        Bn.style.top = calculateResponsiveValue(400, 'top');
+        Bn.style.left = calculateResponsiveValue(50, 'left');
+        flag = 3;
+    } else if (flag == 3) {
+        Bn.style.top = calculateResponsiveValue(370, 'top');
+        Bn.style.left = calculateResponsiveValue(166, 'left');
+        flag = 1;
+    }
+}
+
+function calculateResponsiveValue(value, axis) {
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var maxAxisValue = axis === 'top' ? windowHeight : windowWidth;
+
+    return (value / 1920) * maxAxisValue + 'px';
+}
